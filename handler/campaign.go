0e24fcea7f39bb4701ded6f950975965d8c358af
fathurohmansf <fathurrhm909@gmail.com>
2024-06-05 21:13:16 +0700

    add service untuk ambil data dari repository

--- handler/campaign.go
+++ handler/campaign.go
@@ -4,6 +4,6 @@ package handler
 // INGAT di kerjakan dari bawah dulu
 // tangkap parameter di handler
 // handler ke service
-// service yg menentukan repository mana yg di-call
-// repository, buat dua : FindAll, FindByUserID
+// service yg menentukan repository mana yg di-call (service.go)
+// repository, buat dua : FindAll, FindByUserID(repository.go)
 // db
