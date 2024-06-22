e4b68a0088ce70d09455bb06231db4ce1649e7e2
fathurohmansf <fathurrhm909@gmail.com>
2024-06-22 23:40:25 +0700

    add Service Campaign Transaction API di service.go

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,21 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionByCampaignID(campaignID int) ([]Transaction, error) {
+	transaction, err := s.repository.GetByCampaignID(campaignID)
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
+}
