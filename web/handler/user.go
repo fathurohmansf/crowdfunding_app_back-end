0b5f6cafe7dfd1dfe7f9406d95fedd99e6cc7a5b
fathurohmansf <fathurrhm909@gmail.com>
2024-08-05 21:22:57 +0700

    add Avatar(foto profile) di page list users CMS

--- web/handler/user.go
+++ web/handler/user.go
@@ -16,18 +16,18 @@ func NewUserHandler(userService user.Service) *userHandler {
 }
 
 func (h *userHandler) Index(c *gin.Context) {
-	// users, err := h.userService.GetAllUsers()
-	// if err != nil {
-	// 	c.HTML(http.StatusInternalServerError, "error.html", nil)
-	// 	return
-	// }
-
-	// Test page Error handling
-	users, _ := h.userService.GetAllUsers()
-	if true {
-		//later untuk handling error
+	users, err := h.userService.GetAllUsers()
+	if err != nil {
 		c.HTML(http.StatusInternalServerError, "error.html", nil)
 		return
 	}
+
+	// Test page Error handling
+	// users, _ := h.userService.GetAllUsers()
+	// if true {
+	// 	//later untuk handling error
+	// 	c.HTML(http.StatusInternalServerError, "error.html", nil)
+	// 	return
+	// }
 	c.HTML(http.StatusOK, "user_index.html", gin.H{"users": users}) //pakai map gin.H untuk bisa akses ke var users, dgn key(untuk template) & value(dari service)
 }
